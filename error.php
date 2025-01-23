0f786c9f6ed9878e06877abb6eb72a4e8f972c7e
Horatio <hs@horatio.cn>
2025-01-23 21:18:57 +0800

    style: refresh error page

--- error.php
+++ error.php
@@ -1,1 +1,23 @@
-<h1>请使用NFC标签进入</h1>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>使用 NFC 标签进入</title>
+    <style>
+        body {
+            display: flex; /* 使用 Flexbox 布局 */
+            justify-content: center; /* 水平居中 */
+            align-items: center; /* 垂直居中 */
+            height: 100vh; /* 设置高度为视口高度 */
+            margin: 0; /* 移除默认的外边距 */
+        }
+        h1 {
+            text-align: center; /* 文本居中 */
+        }
+    </style>
+</head>
+<body>
+    <h1>请使用 NFC 标签进入</h1>
+</body>
+</html>
