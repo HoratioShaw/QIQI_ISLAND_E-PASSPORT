70d1de61e458969e26cd3ab5ff588c1b2e8b76e1
Horatio <hs@horatio.cn>
2025-01-23 21:21:07 +0800

    chore: update

--- error.php
+++ error.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>使用 NFC 标签进入</title>
+    <title>QIQI ISLAND PASSPORT</title>
     <style>
         body {
             display: flex; /* 使用 Flexbox 布局 */
