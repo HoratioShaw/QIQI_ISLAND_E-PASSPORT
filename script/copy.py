8827aaebf6224155ea6468cfdaf3192a459bc8ed
Horatio <hs@horatio.cn>
2025-02-13 19:40:15 +0800

    fix: bug fix

--- script/copy.py
+++ script/copy.py
@@ -1,24 +1,44 @@
 import os
 import shutil
 
-source_dir = '../passport/template'
-target_root_dir = '../passport'
+source_folder = os.path.abspath("../passport/template")
+target_parent_folder = os.path.abspath("../passport")
 
-for filename in os.listdir(source_dir):
-    if filename == 'avatar.jpg':
-        continue
+excluded_files = ["avatar.jpg"]
 
-    source_file_path = os.path.join(source_dir, filename)
+source_files = []
+for root, dirs, files in os.walk(source_folder):
+    for file in files:
+        if file not in excluded_files:
+            source_files.append(os.path.join(root, file))
 
-    if os.path.isfile(source_file_path):
-        for subfolder in os.listdir(target_root_dir):
-            target_folder_path = os.path.join(target_root_dir, subfolder)
+target_folders = []
+for item in os.listdir(target_parent_folder):
+    item_path = os.path.join(target_parent_folder, item)
+    if os.path.isdir(item_path) and item_path != source_folder:
+        target_folders.append(item_path)
 
-            if os.path.isdir(target_folder_path):
-                target_file_path = os.path.join(target_folder_path, filename)
+for target_folder in target_folders:
+    for source_file in source_files:
+        file_name = os.path.basename(source_file)
+        target_file = os.path.join(target_folder, file_name)
 
-                env_file_path = os.path.join(target_folder_path, '.env')
-                if not os.path.exists(env_file_path):
-                    shutil.copy2(source_file_path, target_file_path)
+        if file_name == ".env":
+            env_file_path = os.path.join(target_folder, ".env")
+            if os.path.exists(env_file_path):
+                continue
+            else:
+                shutil.copy2(source_file, target_file)
+                folder_name = os.path.basename(target_folder)
+                with open(target_file, 'r+') as f:
+                    lines = f.readlines()
+                    f.seek(0)
+                    for line in lines:
+                        if line.startswith("PASSPORT_NUMBER="):
+                            line = f"PASSPORT_NUMBER={folder_name}\n"
+                        f.write(line)
+                    f.truncate()
+        else:
+            shutil.copy2(source_file, target_file)
 
 print("文件复制完成！")
