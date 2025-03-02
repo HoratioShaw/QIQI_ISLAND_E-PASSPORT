32d545c14f44d2d877cc3c00ad7c54cfbd8b831a
Horatio <hs@horatio.cn>
2025-03-02 12:19:28 +0800

    refactor: synchronous design

--- script/mkdir.py
+++ script/mkdir.py
@@ -5,7 +5,7 @@ target_dir = "../passport"
 os.makedirs(target_dir, exist_ok=True)
 
 for i in range(1, 1001):
-    folder_name = f"{i:04d}"
+    folder_name = f"{i:07d}"
     folder_path = os.path.join(target_dir, folder_name)
 
     if not os.path.exists(folder_path):
--- script/token.py
+++ script/token.py
@@ -7,7 +7,7 @@ def generate_token(length=12):
 
 with open(".env", "w") as env_file, open("links.txt", "w") as links_file:
     for i in range(1, 1001):
-        token_id = f"{i:04d}"
+        token_id = f"{i:07d}"
         token_value = generate_token()
 
         env_file.write(f"TOKEN_{token_id}={token_value}\n")
